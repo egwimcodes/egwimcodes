1d9d3d60b9bee61534ee84ad283619b0ba8b80fe
egwimcodes <egwimcodes@gmail.com>
2024-01-07 19:50:30 +0100

    completed services section

--- src/containers/services/Services.tsx
+++ src/containers/services/Services.tsx
@@ -1,9 +1,36 @@
 
 function Services() {
   return (
-    <div>
-      
-    </div>
+   <>
+   <section className="services" id="services">
+     <h2 className="heading">Our <span>Services</span></h2>
+     <div className="services-container">
+       <div className="services-box">
+         <i className="bx bx-code-alt bx-tada bx-flip-horizontal"></i>
+         <h3>Web Development</h3>
+         <p>High level experience in web design and development knowledge, producing quality work.</p>
+         <a href="#" className="btn">Read More</a>
+       </div>
+
+       <div className="services-box">
+         <i className="bx bx-code-alt bx-tada bx-flip-horizontal"></i>
+         <h3>App Development</h3>
+         <p>High level experience in web design and development knowledge, producing quality work.</p>
+         <a href="#" className="btn">Read More</a>
+       </div>
+
+       <div className="services-box">
+         <i className="bx bx-code-alt bx-tada bx-flip-horizontal"></i>
+         <h3>Software Development</h3>
+         <p>High level experience in web design and development knowledge, producing quality work.</p>
+         <a href="#" className="btn">Read More</a>
+       </div>
+
+     </div>
+
+
+   </section>
+   </>
   )
 }
 
