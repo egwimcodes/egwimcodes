ee4495b63d3b96ccdcd57b2c1b326f66ee37a2a5
egwimcodes <egwimcodes@gmail.com>
2024-01-07 20:44:31 +0100

    completed contact Me section

--- src/containers/contact/Contact.tsx
+++ src/containers/contact/Contact.tsx
@@ -1,9 +1,23 @@
 
 function Contact() {
   return (
-    <div>
+    <>
+    <section className="contact" id="contact">
+      <h2 className="heading">Contact <span>Me</span></h2>
       
-    </div>
+      <form action="#"> 
+        <div className="input-box">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Your Email" />
+        </div>
+        <div className="input-box">
+          <input type="number" placeholder="Your Number" />
+          <input type="text" placeholder="Your Subject" />
+        </div>
+        <textarea name="" id="" cols={30} rows={10} placeholder="Your Message"></textarea>
+        <input type="submit" value="Send Message" className="btn" />
+      </form>
+      </section></>
   )
 }
 
