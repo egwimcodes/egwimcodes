d561813b9a100e353d524603625498833c61a8d4
egwimcodes <egwimcodes@gmail.com>
2024-01-07 19:39:31 +0100

    worked on aboutMe section

--- src/containers/aboutme/Aboutme.tsx
+++ src/containers/aboutme/Aboutme.tsx
@@ -1,10 +1,22 @@
-
+import about from '../../assets/images/about.png'
 function Aboutme() {
   return (
-    <div>
+    <>
+    <section className="about" id="about">
+      <div className="about-img">
+        <img src={about} alt=""/>
+      </div>
       
-    </div>
-  )
+      <div className="about-content">
+        <h2 className="heading">About <span>Me</span></h2>
+        <h3>Full Stack Developer</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci quam deleniti harum, mollitia quasi obcaecati, quaerat id quod tempore fugiat exercitationem animi est placeat necessitatibus in vel similique pariatur cumque!
+        Magnam, maiores minima! Est incidunt accusamus doloremque, mollitia qui voluptatem reprehenderit dolore doloribus dolorum nobis labore ipsum magni quo nisi, sed assumenda sit saepe voluptatibus delectus soluta amet ea! Velit.</p>
+        <a href="#" className="btn">Download CV</a>
+      </div>
+    </section>
+    </>
+    )
 }
 
 export default Aboutme
