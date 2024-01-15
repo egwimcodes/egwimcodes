b881b507855059762e3f7ff1a8df29e30b4c0f66
egwimcodes <egwimcodes@gmail.com>
2024-01-15 12:30:08 +0100

    updated some portfolios

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,6 +1,6 @@
 export{ default as portfolio1 } from './portfolio1.png'
 export { default as portfolio2 } from './portfolio2.png'
-export { default as portfolio3 } from './portfolio3.jpg'
+export { default as portfolio3 } from './portfolio3.png'
 export { default as portfolio4 } from './portfolio4.jpg'
 export { default as portfolio5 } from './portfolio5.jpg'
-export { default as portfolio6 } from './portfolio6.jpg'
+export { default as portfolio6 } from './portfolio6.png'
